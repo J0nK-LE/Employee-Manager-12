7ddfbba997cd951e1b77840c2312f137441850dc
Jon Kelly <jonfkelly@gmail.com>
2022-10-04 21:53:13 -0500

    added more questions and started making functions to get tables

--- db/schema.sql
+++ db/schema.sql
@@ -11,16 +11,16 @@ CREATE TABLE department (
 
 CREATE TABLE roles (
   id INT NOT NULL auto_increment primary key,
-  title VARCHAR(30) NOT NULL
-  salary DECIMAL NOT NULL
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INT NOT NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT NOT NULL auto_increment primary key,
-  firstName VARCHAR(30) NOT NULL
-  lastName VARCHAR(30) NOT NULL
-  role_id INT NOT NULL
+  firstName VARCHAR(30) NOT NULL,
+  lastName VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
   manager_id INT NOT NULL
 );
 
--- index.js
+++ index.js
@@ -1,49 +1,159 @@
 const mysql = require('mysql2/promise');
-let inquirer = require("inquirer")
+let inquirer = require("inquirer");
+
 
 let connection
+
+const test = [
+  //   {
+  //    type: 'input',
+  //    message: "write what you need",
+  //    name: 'first_name'
+  // },
+  {
+    type: 'list',
+    message: "write what you need",
+    name: 'testMenuOptions',
+    choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update employee role', "I'm finished"]
+  }
+]
+
+const menuQuestion = [{
+  type: 'list',
+  message: 'What would you like to do?',
+  name: 'menuOptions',
+  choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update employee role', "I'm finished"]
+}]
+
+
+const addDepartments = [{
+  type: 'input',
+  message: 'Name of the department you want to add?',
+  name: 'departmentName'
+}]
+
+const addRole = [{
+  type: 'input',
+  message: 'Name of the role you want to add?',
+  name: 'roleName'
+},
+{
+  type: 'input',
+  message: 'What is the salary of the new role?',
+  name: 'roleSalary'
+},
+{
+  type: 'list',
+  message: 'Which department does this role belong to?',
+  name: 'roleDepartment',
+  choices: ['LIST OF DEPARTMENT ROLES GO HERE']
+},]
+
+const addEmployee = [{
+  type: 'input',
+  message: 'First name of the employee you want to add?',
+  name: 'empFirstName'
+},
+{
+  type: 'input',
+  message: 'Last name of the employee you want to add?',
+  name: 'empLastName'
+},
+{
+  type: 'input',
+  message: 'What is the employees role?',
+  name: 'empRole',
+  choices: ['LIST OF ROLES GO HERE']
+},
+{
+  type: 'list',
+  message: 'Who is the employees manager?',
+  name: 'empManager',
+  choices: ['LIST OF MANAGERS GO HERE']
+}]
+
+const updateRole = [{
+  type: 'list',
+  message: 'Which department does this role belong to?',
+  name: 'roleDepartment',
+  choices: ['LIST OF DEPARTMENT ROLES GO HERE']
+}]
 
 initialize()
 main();
 
 
-async function initialize(){
-    connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'employeetracker_db'})
+async function initialize() {
+  connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database: 'employeetracker_db'
+  })
 }
 
 
+
 async function main() {
-    console.log(`,--------------------------------------------------------.
-|                                                        |
-|  ______                 _                              |
-| |  ____|_ ___ __  _ __ | |  ____  _    _   ___  ___    |
-| |  __| | '_ '_  \\|  '_\\| |/  __ \\| |  |  |/ _ \\/ _ \\   |
-| | |____| | | | | | |_) | |  (__) | |__|  |  ___/  __/  |
-| |______|_| |_| |_| .__/|_|\\_____/\\____,  |\\___|\\___|   |
-|                  |_|              |_____/              |
-|     __  __                                             |
-|    |  \\/  | ___ _ _ ___   ___ _  __ __  ___  _ __      |
-|   | |\\/| |/  _' | '__ \\ /  _' |/ _'  |/ _  \\/ __|      |
-|  | |  | |  ( | | |  | |  ( | | ( |  |  ___/  |         |
-| |_|  |_|\\__,__|_|  |_|\\__,__|\\__,  |\\___|___|          |
-|                                |__/                    |
-'--------------------------------------------------------'`)
-    // get the client
-    // create the connection
-    const responseObject = await inquirer.prompt([ {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'menuOptions',
-      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department','add a role', 'add an employee', 'update employee role'],
-      },])
+  console.log(`,--------------------------------------------------------.
+  |                                                        |
+  |  ______                 _                              |
+  | |  ____|_ __ __   _ __ | |  ____  _    _   ___  ___    |
+  | |  __| | '_ '_  \\|  '_\\| |/  __ \\| |  |  |/ _ \\/ _ \\   |
+  | | |____| | | | | | |_) | |  (__) | |__|  |  __/  __/   |
+  | |______|_| |_| |_| .__/|_|\\_____/\\____,  |\\___|\\___|   |
+  |                  |_|              |_____/              |
+  |     __  __                                             |
+  |    |  \\/  | ___ _ _ __   ___ _  __ __   ___  _ __      |
+  |   | |\\/| |/  _' | '__ \\ /  _' |/ _'  |/ _  \\/ __|      |
+  |  | |  | |  ( | | |  | |  ( | | ( |  |  ___/  |         |
+  | |_|  |_|\\__,__|_|  |_|\\__,__|\\__,  |\\___|___|          |
+  |                                |__/                    |
+  '--------------------------------------------------------'`)
+  
+  const menuAnswer = await inquirer.prompt(test)
 
-      console.log(responseObject)
+  console.log(menuAnswer.testMenuOptions)
 
 
-    // query database
-    const [rows] = await connection.execute(`SELECT * FROM employees where firstname = ?`,[responseObject.first_name] );
-    console.table(rows);
-
-
-
+ 
+  switch (menuAnswer.testMenuOptions) {
+    case 'view all departments':
+      viewDepartments()
+      break;
+    case 'view all roles':
+      viewRoles()
+      break;
+    case 'view all employees':
+      viewEmployees()
+      break;
+    default:
+      break;
   }
+
+
+
+
+
+
+}
+
+
+async function viewDepartments() {
+  const [departments] = await connection.execute(`SELECT * FROM department`);
+  console.table(departments);
+}
+
+async function viewRoles() {
+  const [roles] = await connection.execute(`SELECT * FROM roles`);
+  console.table(roles);
+}
+
+async function viewEmployees() {
+  const [employees] = await connection.execute(`SELECT * FROM employees`);
+  console.table(employees);
+}
+
+function runMenu(){
+
+}
